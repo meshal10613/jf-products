357ccaad1bd7dc471ce5b366cb8c512b882f8d85
meshal10613 <syedmohiuddinmeshal@gmail.com>
2025-09-07 11:53:52 +0600

    create next js project & navbar, trusted section done

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+import { AiOutlineUser } from 'react-icons/ai'
+import { CiSearch } from 'react-icons/ci'
+import { IoBagOutline } from 'react-icons/io5'
+
+export default function Navbar() {
+    return (
+        <div className='max-w-7xl mx-2 md:mx-auto flex items-center justify-between py-5'>
+            <div>
+                <ul className='flex items-center gap-3'>
+                    <li><Link href='/home' className='text-base'>Home</Link></li>
+                    <li><Link href='/shop' className='text-base'>Shop</Link></li>
+                    <li><Link href='/blogs' className='text-base'>Blogs</Link></li>
+                    <li><Link href='/contact' className='text-base'>Contact</Link></li>
+                </ul>
+            </div>
+            <div className='flex items-center gap-2 md:gap-5'>
+                <Link href='/login' className='flex items-center gap-1 text-base'>Login <AiOutlineUser size={20} /></Link>
+                <Link href={'/search'} className='text-base'><CiSearch size={20} /></Link>
+                <Link href={'/cart'} className='text-base'><IoBagOutline size={20} /></Link>
+            </div>
+        </div>
+    )
+}
---
+++ app/components/Trusted.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Trusted1 from '@/app/assetes/Trusted/trusted1.png';
+import Trusted2 from '@/app/assetes/Trusted/trusted2.png';
+import Trusted3 from '@/app/assetes/Trusted/trusted3.png';
+import Trusted4 from '@/app/assetes/Trusted/trusted4.png';
+import Image from 'next/image';
+
+export default function Trusted() {
+    return (
+        <div className='bg-[#002B55] max-w-7xl mx-auto rounded-3xl space-y-5 py-20 relative overflow-hidden'>
+            <h2 className='text-[42px] font-medium text-white text-center'>Trusted by over 6K+ customers</h2>
+            <div className='flex flex-col md:flex-row items-center justify-center gap-20'>
+                <div className='flex items-center gap-3'>
+                    <div className='bg-white p-4 rounded-full w-fit'>
+                        <Image src={Trusted1} alt="Trusted1" className=' rounded-full' />
+                    </div>
+                    <h2 className='text-white font-medium text-3xl'>Premium <br /> <span className='text-2xl'>Products</span></h2>
+                </div>
+                <div className='flex items-center gap-3'>
+                    <div className='bg-white p-4 rounded-full w-fit'>
+                        <Image src={Trusted2} alt="Trusted1" className=' rounded-full' />
+                    </div>
+                    <h2 className='text-white font-medium text-3xl'>Premium <br /> <span className='text-2xl'>Products</span></h2>
+                </div>
+                <div className='flex items-center gap-3'>
+                    <div className='bg-white p-4 rounded-full w-fit'>
+                        <Image src={Trusted3} alt="Trusted1" className=' rounded-full' />
+                    </div>
+                    <h2 className='text-white font-medium text-3xl'>Premium <br /> <span className='text-2xl'>Products</span></h2>
+                </div>
+            </div>
+            <Image src={Trusted4} alt="Trusted4" className='absolute -top-[58px] left-1/4 z-50' />
+        </div>
+    )
+}
